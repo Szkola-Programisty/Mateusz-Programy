045f4002141becc3bb77c2ffcaacb34af6d4806b
kasa4565 <45254527+kasa4565@users.noreply.github.com>
2020-06-05 10:47:40 +0200

    Tak

---
+++ .vs/KalkulatorBMIgraficznySOLVE/KalkulatorBMIgraficzny/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace KalkulatorBMIgraficzny
+{
+    partial class KalkulatorBMI
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.waga = new System.Windows.Forms.TextBox();
+            this.wynikBMI = new System.Windows.Forms.TextBox();
+            this.wzrost = new System.Windows.Forms.TextBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // waga
+            // 
+            this.waga.Location = new System.Drawing.Point(0, 12);
+            this.waga.Name = "waga";
+            this.waga.Size = new System.Drawing.Size(100, 20);
+            this.waga.TabIndex = 0;
+            // 
+            // wynikBMI
+            // 
+            this.wynikBMI.Location = new System.Drawing.Point(0, 64);
+            this.wynikBMI.Name = "wynikBMI";
+            this.wynikBMI.Size = new System.Drawing.Size(230, 20);
+            this.wynikBMI.TabIndex = 1;
+            this.wynikBMI.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // wzrost
+            // 
+            this.wzrost.Location = new System.Drawing.Point(0, 38);
+            this.wzrost.Name = "wzrost";
+            this.wzrost.Size = new System.Drawing.Size(100, 20);
+            this.wzrost.TabIndex = 2;
+            this.wzrost.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.Apply += new System.EventHandler(this.fontDialog1_Apply);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(72, 136);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 71);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // KalkulatorBMI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 238);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.wzrost);
+            this.Controls.Add(this.wynikBMI);
+            this.Controls.Add(this.waga);
+            this.Name = "KalkulatorBMI";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox waga;
+        private System.Windows.Forms.TextBox wynikBMI;
+        private System.Windows.Forms.TextBox wzrost;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Drawing;
+
+namespace KalkulatorBMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            double KG = 0;
+            double cm = 0;
+            bool weight = false;
+            bool height = false;
+            Console.WriteLine("BMI calculator");
+            Console.WriteLine("---------------\n");
+
+            while (!endApp)
+            {
+                while (!weight)
+                { 
+                    //kg value
+                    Console.Write("Podaj swoją wagę w kg: ");
+
+                try
+                {
+                    KG = Convert.ToInt32(Console.ReadLine());
+                    weight = true;
+                    break;
+                }
+                catch (Exception e)
+                {
+                    
+                    Console.WriteLine("Źle podałeś wartość wagi, spróbuj podać ją w kilogramach.\n - Szczegóły dotyczące błędu: " + e.Message);
+                    
+                }
+            }
+
+                while (!height)
+                {
+                    //kg value
+                    Console.Write("Podaj swój wzrost w cm: ");
+
+                    try
+                    {
+                        cm = Convert.ToInt32(Console.ReadLine());
+                        height = true;
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+
+                        Console.WriteLine("Źle podałeś wartość wzrostu, spróbuj podać ją w centymetrach.\n - Szczegóły dotyczące błędu: " + e.Message);
+
+                    }
+                }
+                //gender pick
+                Console.Write("Podaj swoją płeć (K/M): ");
+                string gender = Console.ReadLine();
+
+                //modula for BMI
+                double BMI = KG / Math.Pow(cm / 100.0, 2);
+                if (gender == "M" || gender == "m")
+                {
+                    BMI = Math.Round(BMI, 2);
+                    Console.WriteLine($"Pana BMI wynosi: {BMI}\n Co oznacza, że: \n");
+                    
+                }
+                else if (gender == "K" || gender == "k")
+                {
+                    BMI = Math.Round(BMI, 2);
+                    Console.WriteLine($"Pani BMI wynosi: {BMI}\n Co oznacza, że: \n");
+                    
+                }
+
+                //doing BMI maths for woman
+                if (gender == "K" || gender == "k")
+                {
+                    if (BMI < 16)
+                    {
+                        Console.WriteLine("Jesteś wygłodzona.");
+                    }
+                    else if (BMI > 16.1 && BMI <= 17.0)
+                    {
+                        Console.WriteLine("Jesteś wychudzona.");
+                    }
+                    else if (BMI > 17.1 && BMI <= 18.5)
+                    {
+                        Console.WriteLine("Masz niedowagę.");
+                    }
+                    else if (BMI > 18.6 && BMI <= 25.0)
+                    {
+                        Console.WriteLine("Masz prawidłową wagę.");
+                    }
+                    else if (BMI > 25.1 && BMI <= 30.0)
+                    {
+                        Console.WriteLine("Masz nadwagę.");
+                    }
+                    else if (BMI > 30.1)
+                    {
+                        Console.WriteLine("Masz poważną nadwagę");
+                    }
+                }
+                //Doing BMI maths for Man
+                if (gender == "M" || gender == "m")
+                {
+                    if (BMI < 16)
+                    {
+                        Console.WriteLine("Jesteś wygłodzony.");
+                    }
+                    else if (BMI > 16.1 && BMI <= 17.0)
+                    {
+                        Console.WriteLine("Jesteś wychudzony.");
+                    }
+                    else if (BMI > 17.1 && BMI <= 18.5)
+                    {
+                        Console.WriteLine("Masz niedowagę.");
+                    }
+                    else if (BMI > 18.6 && BMI <= 25.0)
+                    {
+                        Console.WriteLine("Masz prawidłową wagę.");
+                    }
+                    else if (BMI > 25.1 && BMI <= 30.0)
+                    {
+                        Console.WriteLine("Masz nadwagę.");
+                    }
+                    else if (BMI > 30.1)
+                    {
+                        Console.WriteLine("Masz poważną nadwagę");
+                    }
+                }
+                //solving automatic killing program when maths ends.
+                Console.WriteLine("------------------------\n");
+                Console.Write("Naciśnij 'n' i Enter żeby zamknąć aplikację, albo inny dowolny guzik i enter żeby kontynuować: ");
+                if (Console.ReadLine() == "n") endApp = true;
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
+
